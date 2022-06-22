8d1210722214b27a64c63f64407940fda15dc446
Matías Ezequiel Mendoza <matiasezequiel.qwerty@gmail.com>
2022-06-22 18:44:59 -0300

    percorre array e imprime quantos valores impares estão contidos no array

---
+++ fundamentos/bloco-04-introducao-a-javascript-logica-de-programacao/dia-02-array-e-loop-for/questao6.js
@@ -0,0 +1,15 @@
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [2, 4, 6];
+let quantidadeValoresImpares = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] % 2 !== 0) {
+    quantidadeValoresImpares += 1;
+  }
+}
+
+if (quantidadeValoresImpares === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(quantidadeValoresImpares);
+}
