19607a8495e472d72bfffbd337d8ff4deaeabd22
Matías Ezequiel Mendoza <matiasezequiel.qwerty@gmail.com>
2022-06-22 18:50:15 -0300

    removido comentário desnecessário no código

--- fundamentos/bloco-04-introducao-a-javascript-logica-de-programacao/dia-02-array-e-loop-for/questao6.js
+++ fundamentos/bloco-04-introducao-a-javascript-logica-de-programacao/dia-02-array-e-loop-for/questao6.js
@@ -1,5 +1,4 @@
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let numbers = [2, 4, 6];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let quantidadeValoresImpares = 0;
 
 for (let i = 0; i < numbers.length; i += 1) {
