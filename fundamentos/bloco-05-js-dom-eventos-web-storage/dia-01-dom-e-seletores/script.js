a4a6c3b66eaf6f44ca45697f159c511388bb8fda
Matías Ezequiel Mendoza <matiasezequiel.qwerty@gmail.com>
2022-06-28 18:51:11 -0300

    encapsulado trechos de codigo criando funções para cada requisito

--- fundamentos/bloco-05-js-dom-eventos-web-storage/dia-01-dom-e-seletores/script.js
+++ fundamentos/bloco-05-js-dom-eventos-web-storage/dia-01-dom-e-seletores/script.js
@@ -1,20 +1,39 @@
-let centerContent = document.getElementsByTagName('p')[1];
-centerContent.innerText =
-  'Atuando no mercado de desenvolvimento como um desenvolvedor Web';
-
-let mainContent = document.getElementsByClassName('main-content')[0];
-mainContent.style.backgroundColor = 'rgb(76,164,109';
-
-let middleSquare = document.getElementsByClassName('center-content')[0];
-middleSquare.style.backgroundColor = 'white';
-
-let title = document.getElementsByTagName('h1')[0];
-title.innerText = 'Exercício 5.1 - JavaScript';
-
-let firstParagraph = document.getElementsByTagName('p')[0];
-firstParagraph.innerHTML = firstParagraph.innerHTML.toUpperCase();
-
-let allParagraphs = document.getElementsByTagName('p');
-for (let p of allParagraphs) {
-  console.log(p.innerText);
+function changeText() {
+  let centerContent = document.getElementsByTagName('p')[1];
+  centerContent.innerText =
+    'Atuando no mercado de desenvolvimento como um desenvolvedor Web';
 }
+
+function changeColorToTrybe() {
+  let mainContent = document.getElementsByClassName('main-content')[0];
+  mainContent.style.backgroundColor = 'rgb(76,164,109';
+}
+
+function changeColorToWhite() {
+  let middleSquare = document.getElementsByClassName('center-content')[0];
+  middleSquare.style.backgroundColor = 'white';
+}
+
+function titleCorrect() {
+  let title = document.getElementsByTagName('h1')[0];
+  title.innerText = 'Exercício 5.1 - JavaScript';
+}
+
+function contentToUpperCase() {
+  let firstParagraph = document.getElementsByTagName('p')[0];
+  firstParagraph.innerHTML = firstParagraph.innerHTML.toUpperCase();
+}
+
+function printAllParagraphs() {
+  let allParagraphs = document.getElementsByTagName('p');
+  for (let p of allParagraphs) {
+    console.log(p.innerText);
+  }
+}
+
+changeText();
+changeColorToTrybe();
+changeColorToWhite();
+titleCorrect();
+contentToUpperCase();
+printAllParagraphs();
