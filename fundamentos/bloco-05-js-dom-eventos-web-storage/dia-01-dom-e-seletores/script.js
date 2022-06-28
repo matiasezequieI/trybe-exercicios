679d3dd1742c897e7bd7c5ebcfddc66cc57808aa
Matías Ezequiel Mendoza <matiasezequiel.qwerty@gmail.com>
2022-06-28 18:28:17 -0300

    altera cor de elementos da pagina para cores da Trybe

--- fundamentos/bloco-05-js-dom-eventos-web-storage/dia-01-dom-e-seletores/script.js
+++ fundamentos/bloco-05-js-dom-eventos-web-storage/dia-01-dom-e-seletores/script.js
@@ -1,3 +1,6 @@
 let centerContent = document.getElementsByTagName('p')[1];
 centerContent.innerText =
   'Atuando no mercado de desenvolvimento como um desenvolvedor Web';
+
+let mainContent = document.getElementsByClassName('main-content')[0];
+mainContent.style.backgroundColor = 'rgb(76,164,109';
