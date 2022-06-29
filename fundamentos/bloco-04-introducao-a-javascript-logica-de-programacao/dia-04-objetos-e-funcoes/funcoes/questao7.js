b9dbe831ce59eb008710d84329c82b3a514fc601
Matías Ezequiel Mendoza <matiasezequiel.qwerty@gmail.com>
2022-06-29 11:50:29 -0300

    função que recebe uma string word e outra string ending. Verifica se a string ending é o final da string word

---
+++ fundamentos/bloco-04-introducao-a-javascript-logica-de-programacao/dia-04-objetos-e-funcoes/funcoes/questao7.js
@@ -0,0 +1,13 @@
+function verificaFimPalavra(palavra, fimPalavra) {
+  palavra = palavra.split('');
+  fimPalavra = fimPalavra.split('');
+  controle = true;
+  for (let index = 0; index < fimPalavra.length; index += 1) {
+    if (
+      palavra[palavra.length - fimPalavra.length + index] != fimPalavra[index]
+    ) {
+      controle = false;
+    }
+  }
+  return controle;
+}
