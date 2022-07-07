1e080e28f6979e0439af9a89f5514015c07c6119
Matías Ezequiel Mendoza <matiasezequiel.qwerty@gmail.com>
2022-07-06 21:04:26 -0300

    adicionado evento para envio de informações do formulário

--- fundamentos/bloco-06-html-css-forms-flexbox-e-responsivo/dia-01-forms/script.js
+++ fundamentos/bloco-06-html-css-forms-flexbox-e-responsivo/dia-01-forms/script.js
@@ -1,3 +1,7 @@
-#title {
-  
-}
+const btnEnviar = document.getElementById('btn-enviar');
+const btnLimpar = document.getElementById('btn-limpar');
+const form = document.querySelector('form');
+
+btnEnviar.addEventListener('click', function (event) {
+  event.preventDefault();
+});
