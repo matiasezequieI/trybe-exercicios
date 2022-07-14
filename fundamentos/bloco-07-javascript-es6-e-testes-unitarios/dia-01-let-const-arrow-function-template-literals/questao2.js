2a342ffea3592fc497b799dddd23435241bca229
Matías Ezequiel Mendoza <matiasezequiel.qwerty@gmail.com>
2022-07-14 18:33:31 -0300

    criado função para ordenar array utilizando sort

---
+++ fundamentos/bloco-07-javascript-es6-e-testes-unitarios/dia-01-let-const-arrow-function-template-literals/questao2.js
@@ -0,0 +1,4 @@
+const oddsAndEvens = (array) => array.sort((a, b) => a - b);
+
+const sorteredNumbers = oddsAndEvens([13, 3, 4, 10, 7, 2]);
+console.log(`os números ${sorteredNumbers} estão ordenados de forma crescente`);
