5aba0e94e18ffbf3a25bda0327a89d9e26d5f634
Matías Ezequiel Mendoza <matiasezequiel.qwerty@gmail.com>
2022-07-26 18:33:59 -0300

    ajustado concatenação para atender resultado do exercicio

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-04-reduce/exercicio1.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-04-reduce/exercicio1.js
@@ -4,7 +4,7 @@ const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
 
 const flatten = () =>
   arrays.reduce(
-    (previousValue, currentValue) => currentValue.concat(currentValue),
+    (previousValue, currentValue) => previousValue.concat(currentValue),
     []
   );
 console.log(flatten());
